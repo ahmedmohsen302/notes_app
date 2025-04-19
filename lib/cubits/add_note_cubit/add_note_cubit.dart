e90ccd61d53449ea3eaf302a437abcee4dc9da24
ahmedmohsen302 <ahmedthemohsen@gmail.com>
2025-04-19 03:28:25 +0200

    colors for add note done

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/rendering.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:notes_app/constants.dart';
@@ -6,8 +7,9 @@ import 'package:notes_app/models/note_model.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
-
+  Color color = Color(0xffD29F80);
   addNote(NoteModel note) async {
+    note.color = color.toARGB32();
     emit(AddNoteLoading());
     try {
       var noteBox = Hive.box<NoteModel>(kNotesBox);
---
+++ lib/widgets/colors_list_view.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
+import 'package:notes_app/widgets/color_item.dart';
+
+class ColorsListView extends StatefulWidget {
+  const ColorsListView({super.key});
+
+  @override
+  State<ColorsListView> createState() => _ColorsListViewState();
+}
+
+class _ColorsListViewState extends State<ColorsListView> {
+  List<Color> colors = [
+    Color(0xffD29F80),
+    Color(0xff97866A),
+    Color(0xff735557),
+    Color(0xff604652),
+  ];
+  int currentIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 40 * 2,
+      child: ListView.builder(
+        itemCount: colors.length,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: GestureDetector(
+              onTap: () {
+                currentIndex = index;
+                BlocProvider.of<AddNoteCubit>(context).color = colors[index];
+                setState(() {});
+              },
+              child: ColorItem(
+                color: colors[index],
+                isActive: currentIndex == index,
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
